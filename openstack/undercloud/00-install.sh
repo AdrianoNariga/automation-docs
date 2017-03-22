b98b7204e8fcd0a3bd5a64c05c1532659030805d
root <root@undercloud.kvm>
2017-03-22 10:44:55 -0400

    asdad

--- openstack/undercloud/00-install.sh
+++ openstack/undercloud/00-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 useradd stack
 usermod -p '$5$ErGmpUVP$k7x4lrYHXkcRYPY4JTH/GwCxzJFO9RdTuyNcil9o9Q0' stack
-echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/stack
+echo "stack ALL=(root) NOPASSWD:ALL" > /etc/sudoers.d/stack
 chmod 0440 /etc/sudoers.d/stack
+cp * /home/stack
 chown -R stack. /home/stack
 yum install -y virt-install python-tripleoclient rhosp-director-images rhosp-director-images-ipa
 su - stack
