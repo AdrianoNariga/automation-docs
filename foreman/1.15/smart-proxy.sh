11c8be95750fbe4ab65c112cb9fe2f00bb90e950
Nariga CDO <narigacdo@gmail.com>
2017-05-22 19:47:20 -0300

    deploy smart proxy

--- foreman/1.15/smart-proxy.sh
+++ foreman/1.15/smart-proxy.sh
@@ -12,6 +12,7 @@ yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install https://yum.theforeman.org/releases/1.15/el7/x86_64/foreman-release.rpm
 yum -y install foreman-installer
+setenforce permissive
 
 clear_proxy(){
 foreman-installer \
