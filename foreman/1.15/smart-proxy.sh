816aead49be5979485a1a8d6c2d029cdf93a71e3
root <root@foreman.internal>
2017-05-23 01:18:20 -0300

    vafdvfdv

--- foreman/1.15/smart-proxy.sh
+++ foreman/1.15/smart-proxy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 foreman_hostname=foreman.internal
 ip_foreman=192.168.111.100
-consumer_key="rz3QBDiSkuRG9qe5DNbNhSrBUYnhYAQ8"
-consumer_secret="MqMj5g7sWf6MT3PjZr6gYvJ68eCS8zFN"
+consumer_key="AthHX2kCeWXA7xaEkxHxDt36bwhkmsJ8"
+consumer_secret="TiAgioyMawKwvFKagWkdabNY2cqk3Hrr"
+
+proxy_hostname=192.168.111.101
+foreman_proxy=proxy.internal
+
 # Pegar essas entradas no foreman server
 # garantir o foreman server resolve o nome do proxy
 #foreman-installer --help | grep oauth-consumer
 
 grep $foreman_hostname /etc/hosts || echo "$ip_foreman $foreman_hostname foreman" >> /etc/hosts
+grep $proxy_hostname /etc/hosts || echo "$ip_proxy $foreman_proxy foreman" >> /etc/hosts
 yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install https://yum.theforeman.org/releases/1.15/el7/x86_64/foreman-release.rpm
@@ -33,6 +38,10 @@ foreman-installer \
   --foreman-proxy-oauth-consumer-key=$consumer_key \
   --foreman-proxy-oauth-consumer-secret=$consumer_secret
 }
+
+scp root@$ip_foreman:/etc/puppetlabs/puppet/ssl/certs/ca.pem /etc/puppetlabs/puppet/ssl/certs/ca.pem
+scp root@$ip_foreman:/etc/puppetlabs/puppet/ssl/certs/foreman.internal.pem /etc/puppetlabs/puppet/ssl/certs/foreman.internal.pem
+scp root@$ip_foreman:/etc/puppetlabs/puppet/ssl/private_keys/foreman.internal.pem /etc/puppetlabs/puppet/ssl/private_keys/foreman.internal.pem
 
 dns_proxy(){
 foreman-installer \
