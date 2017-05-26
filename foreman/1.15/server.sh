6547959b0b0bf1afca1d866ee976124d2f66a185
root <root@foreman.internal>
2017-05-26 00:21:21 -0300

    dsadfsd

--- foreman/1.15/server.sh
+++ foreman/1.15/server.sh
@@ -2,11 +2,11 @@
 foreman_hostname=foreman.internal
 ip_foreman=192.168.111.100
 
-proxy_hostname=192.168.111.101
-foreman_proxy=proxy.internal
+proxy_hostname=proxy.internal
+ip_proxy=192.168.111.101
 
-grep $foreman_hostname /etc/hosts || echo "$ip_foreman $foreman_hostname foreman" >> /etc/hosts
-grep $proxy_hostname /etc/hosts || echo "$ip_proxy $foreman_proxy foreman" >> /etc/hosts
+grep $foreman_hostname /etc/hosts || echo "$ip_foreman $foreman_hostname" >> /etc/hosts
+grep $proxy_hostname /etc/hosts || echo "$ip_proxy $proxy_hostname" >> /etc/hosts
 yum -y install https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 yum -y install http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 yum -y install https://yum.theforeman.org/releases/1.15/el7/x86_64/foreman-release.rpm
