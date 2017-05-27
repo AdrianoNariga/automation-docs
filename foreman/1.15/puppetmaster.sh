e997486ba2ab5f1a55a16a3f285899d986a476f7
Your Name <you@example.com>
2017-05-27 02:07:46 -0300

    iniciando puppet standalone

---
+++ foreman/1.15/puppetmaster.sh
@@ -0,0 +1,10 @@
+foreman-installer \
+  --no-enable-foreman \
+  --no-enable-foreman-cli \
+  --no-enable-foreman-plugin-bootdisk \
+  --no-enable-foreman-plugin-setup \
+  --enable-puppet \
+  --puppet-server-ca=false \
+  --puppet-server-foreman-url=https://foreman.example.com \
+  --enable-foreman-proxy \
+  --foreman-proxy-puppetca=false \
