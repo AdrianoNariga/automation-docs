61c7b5bee4f5acd0d0573d08f0f4a76c31ca6c54
Adriano Nariga <nariga@ideapad.home.ps>
2022-03-06 16:03:30 -0300

    using live cd to work

--- so/install-anaconda.sh
+++ so/install-anaconda.sh
@@ -6,7 +6,7 @@ wget https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh \
 	-O /tmp/anaconda.sh
 bash /tmp/anaconda.sh -b -p /opt/anaconda
 
-group add anaconda
+groupadd anaconda
 chgrp -R anaconda /opt/anaconda
 chmod 770 -R /opt/anaconda
 adduser nariga anaconda
---
+++ ubt-live-work.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cat > /etc/apt/sources.list.d/ << EOF
+deb http://archive.ubuntu.com/ubuntu/ impish main restricted multiverse universe
+deb http://security.ubuntu.com/ubuntu/ impish-security main restricted multiverse universe
+deb http://archive.ubuntu.com/ubuntu/ impish-updates main restricted multiverse universe
+EOF
+
+apt update
+
+apt install vim openssh-server curl dislocker barrier -y
+
+mkdir -p /media/bitlocker
+mkdir -p /media/bitlockermount
+
+dislocker /dev/sda4 -u134266-246697-280346-203720-186230-046266-701019-656524 -- /media/bitlocker
+
+barrierc -f --no-tray --debug INFO --name ubuntu [192.168.111.65]:24800
